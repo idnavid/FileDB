5605eda88782e8956b95c56969ee69e8fa75728e
idnavid <navidshokouhi68@gmail.com>
2025-04-28 23:00:33 +1000

    Initial commit for FileDB library

--- Sources/FileDB/FileDB.swift
+++ Sources/FileDB/FileDB.swift
@@ -1,2 +1,54 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class SimpleFileDB {
+    let baseURL: URL
+
+    public init(baseDirectory: String) {
+        let manager = FileManager.default
+        let docs = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        self.baseURL = docs.appendingPathComponent(baseDirectory)
+
+        if !manager.fileExists(atPath: baseURL.path) {
+            try? manager.createDirectory(at: baseURL, withIntermediateDirectories: true)
+        }
+    }
+
+    public func createFile(named name: String, contents: String) {
+        let fileURL = baseURL.appendingPathComponent(name)
+        try? contents.write(to: fileURL, atomically: true, encoding: .utf8)
+    }
+
+    public func readFile(named name: String) -> String? {
+        let fileURL = baseURL.appendingPathComponent(name)
+        return try? String(contentsOf: fileURL)
+    }
+
+    public func appendToFile(named name: String, text: String) {
+        let fileURL = baseURL.appendingPathComponent(name)
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            if var contents = try? String(contentsOf: fileURL) {
+                contents += text
+                try? contents.write(to: fileURL, atomically: true, encoding: .utf8)
+            }
+        } else {
+            createFile(named: name, contents: text)
+        }
+    }
+
+    public func listFiles() -> [String] {
+        return (try? FileManager.default.contentsOfDirectory(atPath: baseURL.path)) ?? []
+    }
+
+    public func deleteFile(named name: String) {
+        let fileURL = baseURL.appendingPathComponent(name)
+        try? FileManager.default.removeItem(at: fileURL)
+    }
+    
+    public func showPath(forFile named: String) -> String {
+        let fileURL = baseURL.appendingPathComponent(named)
+        return fileURL.path
+    }
+}
--- Tests/FileDBTests/FileDBTests.swift
+++ Tests/FileDBTests/FileDBTests.swift
@@ -5,6 +5,15 @@ final class FileDBTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
+        let db = SimpleFileDB(baseDirectory: "TestDB")
+
+        db.createFile(named: "test.txt", contents: "Hello World")
+        print(db.readFile(named: "test.txt") ?? "Nothing")
+        db.appendToFile(named: "test.txt", text: "\nSecond Line")
+        print(db.readFile(named: "test.txt") ?? "Nothing")
+        print(db.listFiles())
+        print(db.showPath(forFile: "test.txt"))
+        db.deleteFile(named: "test.txt")
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
